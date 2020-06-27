77955f3c35e6f8fd1c9b051acf328d25e8064c79
DiogodePaula <depaulatrabalhos@gmail.com>
2020-06-27 16:41:58 -0300

    pronto

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function onlyVowels( Programacao ){
     return Programacao.replace(/[B C D F G J K L M N P Q R S T V W X Z]/gi,'');
 }
 
-const resultado = onlyVowels( "Todo dia aprendendo um pouco mais" );
+const strVowels = onlyVowels( "Todo dia aprendendo um pouco mais" );
 
-console.log( resultado ); 
+console.log(strVowels); 
